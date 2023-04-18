af1ca1513da32d5cd1f84e5174099628331634ec
yun <austru00@gmail.com>
2023-04-18 16:13:55 +0900

    6.0 Introduction

---
+++ 6EFFECTS/react-for-beginners/src/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  return (
+    <div>
+      <h1> {counter}</h1>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
